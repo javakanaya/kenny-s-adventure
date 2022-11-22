7689f2a243ac70a57100de3f5cab186a038dea06
jap <javakanaya@gmail.com>
2022-11-22 22:38:19 +0700

    EventRect class, one time event setting

--- src/com/kenny/main/EventHandler.java
+++ src/com/kenny/main/EventHandler.java
@@ -1,78 +1,90 @@
 package com.kenny.main;
 
-import java.awt.Rectangle;
-
 public class EventHandler {
 
     GamePanel gp;
-    Rectangle eventRect;
-    int eventRectDefaultX, eventRectDefaultY;
-
+    EventRect eventRect[][];
+    
     public EventHandler(GamePanel gp) {
+
         this.gp = gp;
 
-        eventRect = new Rectangle();
-        eventRect.x = 20;
-        eventRect.y = 20;
-        eventRect.width = 8;
-        eventRect.height = 8;
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultY = eventRect.y;
+        // event for each tiles on worldMap
+        eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
+
+        int col = 0;
+        int row = 0;
+        while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
+            eventRect[col][row] = new EventRect();
+            eventRect[col][row].x = 20;
+            eventRect[col][row].y = 20;
+            eventRect[col][row].width = 8;
+            eventRect[col][row].height = 8;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+            eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if (col == gp.maxWorldCol) {
+                col = 0;
+                row++;
+            }
+
+        }
     }
 
     public void checkEvent() {
 
-//        if(hit(51, 51, "any") == true) {
-//            damagePit(gp.dialogueState);
-//        }
+        if (hit(51, 51, "any") == true) {
+            damagePit(gp.dialogueState, 51, 51);
+        }
 //        if(hit(51, 60, "any") == true) {
 //            healingPool(gp.dialogueState);
 //        }
-    	
-    	// pillar1
+
+        // pillar1
         if (hit(90, 53, "left") == true) {
             teleport(gp.dialogueState, 78, 48);
         }
         if (hit(78, 48, "right") == true) {
-        	teleport(gp.dialogueState, 90, 53);
+            teleport(gp.dialogueState, 90, 53);
         }
-        
-     // pillar2
+
+        // pillar2
         if (hit(80, 56, "left") == true) {
             teleport(gp.dialogueState, 86, 43);
         }
         if (hit(86, 43, "right") == true) {
-        	teleport(gp.dialogueState, 80, 56);
+            teleport(gp.dialogueState, 80, 56);
         }
-        
-     // pillar3
+
+        // pillar3
         if (hit(79, 40, "left") == true) {
             teleport(gp.dialogueState, 88, 49);
         }
         if (hit(88, 49, "right") == true) {
-        	teleport(gp.dialogueState, 79, 40);
+            teleport(gp.dialogueState, 79, 40);
         }
-        
-     // pillar4
+
+        // pillar4
         if (hit(93, 45, "left") == true) {
             teleport(gp.dialogueState, 82, 51);
         }
         if (hit(82, 51, "right") == true) {
-        	teleport(gp.dialogueState, 93, 45);
+            teleport(gp.dialogueState, 93, 45);
         }
-        
-     // key
+
+        // key
         if (hit(74, 45, "left") == true) {
             teleport(gp.dialogueState, 83, 46);
         }
         if (hit(83, 46, "right") == true) {
-        	teleport(gp.dialogueState, 74, 45);
+            teleport(gp.dialogueState, 74, 45);
         }
-        
+
     }
 
     // check event collision
-    public boolean hit(int eventCol, int eventRow, String reqDirection) {
+    public boolean hit(int col, int row, String reqDirection) {
 
         boolean hit = false;
 
@@ -80,11 +92,11 @@ public class EventHandler {
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
 
-        eventRect.x = (eventCol * gp.tileSize) + eventRect.x;
-        eventRect.y = (eventRow * gp.tileSize) + eventRect.y;
+        eventRect[col][row].x = (col * gp.tileSize) + eventRect[col][row].x;
+        eventRect[col][row].y = (row * gp.tileSize) + eventRect[col][row].y;
 
         // checking if player's solidArea is colliding with eventRect's solidArea
-        if (gp.player.solidArea.intersects(eventRect)) {
+        if (gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
             // check player direction so event only happened when player facing some
             // direction
             if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
@@ -96,23 +108,25 @@ public class EventHandler {
         // after checking the collision reset the solidArea x and y
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
         return hit;
     }
 
-    public void teleport(int gameState, int destinationX, int destinationY) {
-            gp.gameState = gameState;
-            gp.ui.currentDialogue = "Teleport!";
-            
-            gp.player.worldX = destinationX * gp.tileSize;
-            gp.player.worldY = destinationY * gp.tileSize;
+    public void teleport(int gameState, int destinationCol, int destinationRow) {
+        gp.gameState = gameState;
+        gp.ui.currentDialogue = "Teleport!";
+
+        gp.player.worldX = destinationCol * gp.tileSize;
+        gp.player.worldY = destinationRow * gp.tileSize;
     }
 
-    public void damagePit(int gameState) {
-            gp.gameState = gameState;
-            gp.ui.currentDialogue = "you fall into a pit";
+    // one time event
+    public void damagePit(int gameState, int col, int row) {
+        gp.gameState = gameState;
+        gp.ui.currentDialogue = "you fall into a pit";
+        eventRect[col][row].eventDone = true;
 
     }
 //    
